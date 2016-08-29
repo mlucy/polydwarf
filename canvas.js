46f61d38559401d66ce831a965ea5b95db6cbf4b
Marc Hesse <marc.hesse@gmail.com>
2016-08-28 21:57:08 -0700

    Fixed panning and zooming
    To be backwards

--- canvas.js
+++ canvas.js
@@ -2,14 +2,30 @@ const canvas = document.getElementById('polydwarf-world');
 const ctx = canvas.getContext('2d');
 let width;
 let height;
-let worldChanged;
 
-ctx.font = '20px monospace';
-ctx.textBaseline = 'middle';
-const cellSize = { y: 10, x: 10 };
-
+let worldChanged = true;
 const world = new World();
-center = { x: 512, y: 512, z: 0 };
+
+function resize() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  worldChanged = true;
+};
+
+resize();
+window.addEventListener('resize', resize);
+
+const cellSize = { x: 10, y: 10 };
+
+// Offsets measured in pixels from top-left
+const screenPos = {
+  x: ((cellSize.x * worldWidth) - width) / 2,
+  y: ((cellSize.y * worldHeight) - height) / 2,
+  z: 0,
+};
 
 const colorTable = [];
 for (let i = 0; i < 256; ++i) {
@@ -21,23 +37,19 @@ function drawWorld() {
   if (worldChanged) {
     ctx.clearRect(0, 0, width, height);
     const startTime = new Date();
-    const numCells =
-      { x: Math.ceil(width / cellSize.x), y: Math.ceil(height / cellSize.y) };
     const renderStart =
-      { x: Math.floor(center.x - numCells.x), y: Math.floor(center.y - numCells.y) };
+      { x: Math.floor(screenPos.x / cellSize.x), y: Math.floor(screenPos.y / cellSize.y) };
     const renderEnd =
-      { x: Math.ceil(center.x + numCells.x), y: Math.ceil(center.y + numCells.y) };
-    const pixelOffset =
-      { x: Math.floor(width / 2), y: Math.floor(height / 2) };
+      { x: renderStart.x + Math.ceil(width / cellSize.x), y: renderStart.y + Math.ceil(height / cellSize.y) };
 
-    console.log(`renderStart: ${JSON.stringify(renderStart)}, renderEnd: ${JSON.stringify(renderEnd)}, numCells: ${JSON.stringify(numCells)}`);
+    console.log(`screenPos: ${JSON.stringify(screenPos)}, renderStart: ${JSON.stringify(renderStart)}, renderEnd: ${JSON.stringify(renderEnd)}`);
     for (let y = renderStart.y; y < renderEnd.y; ++y) {
       for (let x = renderStart.x; x < renderEnd.x; ++x) {
-        const b = world.bytes[center.z][y][x];
+        const b = world.bytes[screenPos.z][y][x];
         if (b !== 0) {
           ctx.fillStyle = colorTable[b];
-          ctx.fillRect(pixelOffset.x + (x - center.x) * cellSize.x,
-                       pixelOffset.y + (y - center.y) * cellSize.y,
+          ctx.fillRect(cellSize.x * x - screenPos.x,
+                       cellSize.y * y - screenPos.y,
                        cellSize.x, cellSize.y);
         }
       }
@@ -48,24 +60,15 @@ function drawWorld() {
   }
 }
 
-function resize() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  canvas.width = width;
-  canvas.height = height;
-
-  worldChanged = true;
-};
-
 function keyDown(event) {
   console.log(`keyDown: ${event.key}`);
   switch (event.key) {
   case 'ArrowUp':
-    center.z = Math.max(0, center.z - 1);
+    screenPos.z = Math.max(0, screenPos.z - 1);
     worldChanged = true;
     break;
   case 'ArrowDown':
-    center.z = Math.min(worldLayers - 1, center.z + 1);
+    screenPos.z = Math.min(worldLayers - 1, screenPos.z + 1);
     worldChanged = true;
     break;
   default:
@@ -76,35 +79,45 @@ function keyDown(event) {
 const zoom_in_ratio = 1.3;
 const zoom_out_ratio = 1 / zoom_in_ratio;
 
+function doZoom(cursorPos, ratio) {
+  const oldWorldPos = {
+    x: (screenPos.x + cursorPos.x) / cellSize.x,
+    y: (screenPos.y + cursorPos.y) / cellSize.y,
+  };
+
+  if (ratio < 1) {
+    cellSize.x = Math.max(Math.min(Math.floor(cellSize.x * ratio), cellSize.x - 1), 1);
+    cellSize.y = Math.max(Math.min(Math.floor(cellSize.y * ratio), cellSize.y - 1), 1);
+  } else if (ratio > 1) {
+    cellSize.x = Math.max(Math.floor(cellSize.x * ratio), cellSize.x + 1);
+    cellSize.y = Math.max(Math.floor(cellSize.y * ratio), cellSize.x + 1);
+  }
+
+  const newWorldPos = {
+    x: (screenPos.x + cursorPos.x) / cellSize.x,
+    y: (screenPos.y + cursorPos.y) / cellSize.y,
+  };
+
+  screenPos.x = screenPos.x - Math.round((newWorldPos.x - oldWorldPos.x) * cellSize.x);
+  screenPos.y = screenPos.y - Math.round((newWorldPos.y - oldWorldPos.y) * cellSize.y);
+  worldChanged = true;
+}
+
 function wheel(event) {
   if (event.deltaY < 0) {
-    cellSize.x = Math.floor(cellSize.x * zoom_in_ratio);
-    cellSize.y = Math.floor(cellSize.y * zoom_in_ratio);
-    worldChanged = true;
+    doZoom({x: event.clientX, y: event.clientY}, zoom_in_ratio);
   } else if (event.deltaY > 0) {
-    cellSize.x = Math.max(5, Math.ceil(cellSize.x * zoom_out_ratio));
-    cellSize.y = Math.max(5, Math.ceil(cellSize.y * zoom_out_ratio));
-    worldChanged = true;
+    doZoom({x: event.clientX, y: event.clientY}, zoom_out_ratio);
   }
 }
 
 let startDrag = null;
 function mouseMove(event) {
   if (startDrag) {
-    let moved = false;
-    const delta = { x: event.clientX - startDrag.x, y: event.clientY - startDrag.y };
-    if (Math.abs(delta.x) > cellSize.x) {
-      center.x += delta.x / cellSize.x;
-      moved = true;
-    }
-    if (Math.abs(delta.y) > cellSize.y) {
-      center.y += delta.y / cellSize.y;
-      moved = true;
-    }
-    if (moved) {
-      startDrag = { x: event.clientX, y: event.clientY };
-      worldChanged = true;
-    }
+    screenPos.x += startDrag.x - event.clientX;
+    screenPos.y += startDrag.y - event.clientY;
+    startDrag = { x: event.clientX, y: event.clientY };
+    worldChanged = true;
   }
 }
 
@@ -141,9 +154,6 @@ canvas.addEventListener('mouseenter', mouseEnter);
 canvas.addEventListener('mouseleave', mouseLeave);
 canvas.addEventListener('mousemove', mouseMove); // TODO: only listen on this when dragging
 
-resize();
-window.addEventListener('resize', resize);
-
 // Disable the right-click menu so users can use right-clicks as input
 canvas.addEventListener('contextmenu', (event) => event.preventDefault());
 
@@ -159,4 +169,3 @@ function animate() {
 };
 
 animate();
-
